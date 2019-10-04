00e0738d066bbea90bceaa4fafdac837e5a63e63
sbedi1 <sbedi@pfsweb.com>
2019-10-04 13:05:16 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
-import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
-import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -13,7 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FontAwesomeModule],
+  imports:      [ BrowserModule, FormsModule, AngularFontAwesomeModule],
   declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
